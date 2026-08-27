5ac874a0e555bee82cf6e1833d118b0f79259b59
Sneha <snehassneha4578@gmail.com>
2026-08-27 11:05:50 +0530

    Delete Day 1 directory

--- Day 1/Packet loss calculation.py
+++
@@ -1,6 +0,0 @@
-total_packets = 1000
-lost_packets = 20
-
-packet_loss = (lost_packets / total_packets) * 100
-
-print("Packet Loss:", packet_loss, "%")
--- Day 1/smart factory calculation.py
+++
@@ -1,4 +0,0 @@
-expected_production = 150
-actual_production = 125
-reduction = expected_production - actual_production
-print("Production reduction:", reduction, "units/hour")
